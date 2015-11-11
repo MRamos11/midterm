e8f67c99dcd3b75ac8c2dc6058165a1682181d20
Mauricio Ramos <mauricio.ramos@f1006-08-34161.bmcc.cuny.edu>
2015-11-10 21:45:01 -0500

    midterm 1st
    clear

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package midterm
+ */
+
+get_header(); ?>
+
+<div class="myMario">
+	<?php myMario(); ?>
+</div>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php endif; ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+					 <div class="post-container">
+							<?php the_title(); ?>
+							<?php the_content(); ?>
+					 </div>
+
+
+			<?php endwhile; ?>
+
+	
+
+			<?php the_posts_navigation(); ?>
+
+
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+
+
+	
+
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+
+
+
