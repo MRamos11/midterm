781660e60c7af41679f0bfef2690f3da08d89727
Mauricio Ramos <mauricio.ramos@f1006-08-34161.bmcc.cuny.edu>
2015-12-03 20:26:00 -0500

    midterm

--- header.php
+++ header.php
@@ -16,8 +16,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<?php wp_enqueue_script("jquery"); ?>
 
 <?php wp_head(); ?>
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -41,3 +43,95 @@
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
+
+
+	<head>
+	<title>More Practice with jQuery</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+	<style>
+body {
+	background-color: none;
+}
+	#redDiv{
+		display: inline-block;
+		background-color: red;
+		width: 200px;
+		height: 200px;
+		margin-top: 80px
+	}	
+	#blueDiv{
+		display:inline-block;
+		margin-left: 40px;
+		background-color: blue;
+		width: 200px;
+		height: 200px;
+		margin-top: 80px;
+	}
+
+	#myCounter{
+		font-size: 5em;
+	}
+
+	#contentWrapper{
+		background-color: #A19EAD;
+		width: 600px;
+		height: 600px;
+		margin-left: auto;
+		margin-right: auto;
+
+	}
+
+	</style>
+</head>
+<body>
+	<div id="pageWrapper">
+		<div id="contentWrapper">
+
+
+			
+				
+			<div id="myCounter"> </div>
+			
+			<div id="redDiv">
+				<p> Click me to count Up</p>
+
+			</div>
+
+			<div id="blueDiv">
+				<p> Click me to count Down</p>
+
+			</div>
+			
+
+			
+
+
+		</div>
+	</div>
+<script type="text/javascript">
+		jQuery(document).ready(function(){
+ 
+		var counter = 0;
+
+		
+
+		jQuery('#redDiv').on('click',function(){
+			counter++;
+			jQuery('#myCounter').html(counter);
+		});
+
+		jQuery('#blueDiv').on('click',function(){
+			counter--;
+			jQuery('#myCounter').html(counter);
+		});
+
+
+	});// document ready
+
+
+
+
+</script>
--- page-about-mario.php
+++ page-about-mario.php
@@ -19,16 +19,30 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
+				<?php the_title(); ?>
+				<?php the_content(); ?>
 
 			<?php endwhile; // End of the loop. ?>
+
+
+			<?php rewind_posts(); ?>
+
+			<?php
+
+				$args = array( 'post_type' => 'about_mario', 'posts_per_page' => 10 );
+				$loop = new WP_Query( $args );
+				while ( $loop->have_posts() ) : $loop->the_post();
+
+				
+				echo '<div class="am-entry-content">';
+				the_content();
+				echo '</div>';
+
+				endwhile;
+
+			?>
+
+			
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
